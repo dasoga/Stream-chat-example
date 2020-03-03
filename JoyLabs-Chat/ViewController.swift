9f78e059756bcf212a6a21b76a3e94dd59ad1741
Dante Solorio <ddasogaa@gmail.com>
2020-03-02 21:47:16 -0600

    Initial project

---
+++ JoyLabs-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoyLabs-Chat
+//
+//  Created by Dante Solorio on 02/03/20.
+//  Copyright © 2020 Dasoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'JoyLabs-Chat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for JoyLabs-Chat
+    pod 'StreamChat', :git => 'https://github.com/dasoga/stream-chat-swift', :branch => 'test'
+end
